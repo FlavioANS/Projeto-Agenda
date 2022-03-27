f02f85018703a7f3a561c6b4c785ad72b127dd90
Flavio <flavioantonio5@hotmail.com>
2022-03-27 18:25:00 -0300

    usando condicionais

--- contatos/models.py
+++ contatos/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=200)
 
     def __str__(self):
         return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=200)
@@ -15,7 +17,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nome
-
